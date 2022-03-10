4c5c007bf6477e1cdc8332a233b1afb3ab0cd91b
Michael Klein <Michaelklein1997@gmail.com>
2022-03-09 21:04:54 -0600

    refactored all controllers into resolver file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,57 @@
-const {Book, User} = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        getSingleUser: async (parent, { user, params }) => {
+            return User.findOne({
+                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+            });
+        },
     },
     Mutation: {
-        
+        createUser: async (parent, { body }, context) => {
+            const userData = await User.create({ _id: context.user._id });
+            const token = signToken(userData);
+
+            return { token, userData };
+        },
+        login: async (parent, { body }, context) => {
+            const userData = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+
+            if (!userData) {
+                throw new AuthenticationError('No profile with this email found!');
+            };
+
+            const correctPassword = await userData.isCorrectPasword(password);
+
+            if (!correctPassword) {
+                throw new AuthenticationError('Incorrect Password')
+            };
+
+            const token = signToken(userData);
+            return { token, userData };
+        },
+        saveBook: async (parent, {user, body}, context) => {
+            const bookData = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+            )
+            return {bookData}
+        },
+        deleteBook: async (parent, {user, params}) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+            )
+            if (!updatedUser) {
+                throw new AuthenticationError("Couldn't find user with this id")
+            }
+            return {updatedUser}
+        }
     }
 };
 
